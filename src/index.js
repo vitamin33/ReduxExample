49aa869b80564488af54074cce5c6074b857389c
Vitalii Serbyn <vivitseser@gmail.com>
2022-06-05 12:25:42 +0300

    Add debounced video search

--- src/index.js
+++ src/index.js
@@ -29,9 +29,11 @@ class App extends Component {
     }
 
     render() {
+        const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
+
         return (
             <div>
-                <SearchBar onSearchTermChanged={term => this.videoSearch(term)}/>
+                <SearchBar onSearchTermChanged={term => videoSearch(term)}/>
                 <VideoDetail video={this.state.selectedVideo} />
                 <VideoList
                     videos={this.state.videos}
